d7c4cb0f7f283865f12c688d236cd22172c6e34f
shriyans jindal <shriyansjindal40517@gmail.com>
2023-11-03 15:41:19 +0530

    done

---
+++ app.js
@@ -0,0 +1,5 @@
+const box = document.getElementsByClassName("box")
+const result = document.getElementById("result")
+const message = document.getElementById("message")
+const playAgain = document.getElementById("button")
+
