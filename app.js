669a03373b5073196949607a25135fd8235d99a3
shriyans jindal <shriyansjindal40517@gmail.com>
2023-11-03 15:51:39 +0530

    done

--- app.js
+++ app.js
@@ -2,4 +2,22 @@ const box = document.getElementsByClassName("box")
 const result = document.getElementById("result")
 const message = document.getElementById("message")
 const playAgain = document.getElementById("button")
+// console.log(box)
+
+let winningCombination = [ 
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]]
+
+for(let i=0;i<winningCombination.length;i++){
+    for(let j=0;j<winningCombination[i].length;j++){
+        // console.log(winningCombination[i][j])
+
+    }
+}
 
